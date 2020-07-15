eef7fcffce7b4f27777d4dad940d6e009b859822
Zion Amador <amador0523@hotmail.com>
2020-07-15 01:29:00 -0700

    final changes

--- Hello_world.py
+++ Hello_world.py
@@ -10,10 +10,8 @@ import numpy as np;
 
  
 print("Hello World!")
-<<<<<<< HEAD
 print("Commit testing")
-=======
 print("Create a new branch and push")
 
->>>>>>> eee8df7ad4dda9ea202d74b11f03863bf81109a4
+eee8df7ad4dda9ea202d74b11f03863bf81109a4
 
